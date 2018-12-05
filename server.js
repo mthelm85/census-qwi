2375e481c78e72a5800b61f10cd31bbddea69207
Matt Helm <mthelm85@gmail.com>
2018-12-05 17:07:31 -0600

    fixed error in server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express'),
       serveStatic = require('serve-static'),
       path = require('path'),
-      port = process.enc.PORT || 3000
+      port = process.env.PORT || 3000
       app = express()
 
 app.use(serveStatic(path.join(__dirname, '/dist')))
